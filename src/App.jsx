5e1aba8f74d64974d76387b03f93f59db42aa1b0
ADRIANO125 <abdulrahmankadri12@gmail.com>
2025-04-04 23:31:16 +0200

    Fix case-sensitive import for Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
-import Main from './components/Main/main'
+import Main from './components/Main/Main'
 import Footer from './components/Footer/Footer'
 
 const App = () => {
